2c397a3b7f360aacc44721b49e8b88c56631d927
Elijah Rwothoromo <rwothoromoelijah@gmail.com>
2018-04-23 15:49:54 +0300

    chore(front tests): fix front tests
    This commit adds tests to the front tests in files
    `test_add_channel.py`, `test_add_check.py` and `test_add_pushbover.py`.
    
    [Fixes  #156577913]

--- hc/front/tests/test_add_check.py
+++ hc/front/tests/test_add_check.py
@@ -11,4 +11,14 @@ class AddCheckTestCase(BaseTestCase):
         self.assertRedirects(r, "/checks/")
         assert Check.objects.count() == 1
 
-    ### Test that team access works
+    def test_it_works_using_team_access(self):
+       url = "/checks/add/"
+
+       # Logging in as bob, not alice. Bob has team access so this
+       # should work.
+       self.client.login(username="bob@example.org", password="password")
+       r = self.client.post(url)
+
+       c = Check.objects.get()
+       self.assertEqual(c.user, self.alice)
+       self.assertRedirects(r, "/checks/")
