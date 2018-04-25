9f125f49227f9b4f4161f129dbe424309970bec2
rogerokello <rogerokello@gmail.com>
2018-04-25 13:46:11 +0300

    #156577915 Fix API Tests (#5)

--- hc/api/tests/test_list_checks.py
+++ hc/api/tests/test_list_checks.py
@@ -5,6 +5,7 @@ from django.utils.timezone import now
 from hc.api.models import Check
 from hc.test import BaseTestCase
 
+from dateutil import parser
 
 class ListChecksTestCase(BaseTestCase):
 
@@ -33,15 +34,33 @@ class ListChecksTestCase(BaseTestCase):
 
     def test_it_works(self):
         r = self.get()
-        ### Assert the response status code
+        
+        self.assertEqual(r.status_code, 200)
 
         doc = r.json()
         self.assertTrue("checks" in doc)
 
         checks = {check["name"]: check for check in doc["checks"]}
-        ### Assert the expected length of checks
-        ### Assert the checks Alice 1 and Alice 2's timeout, grace, ping_url, status,
-        ### last_ping, n_pings and pause_url
+        
+        self.assertEqual(len(checks), 2)
+
+        #Alice 1
+        self.assertEqual(checks['Alice 1']['timeout'], 3600)
+        self.assertEqual(checks['Alice 1']['grace'], 900)
+        self.assertIn('http://', checks['Alice 1']['ping_url'])
+        self.assertEqual(checks['Alice 1']['status'], 'new')
+        self.assertEqual(self.now, parser.parse(checks['Alice 1']['last_ping']))
+        self.assertEqual(checks['Alice 1']['n_pings'], 1)
+        self.assertIn('pause', checks['Alice 1']['pause_url'])
+
+        #Alice 2
+        self.assertEqual(checks['Alice 2']['timeout'], 86400)
+        self.assertEqual(checks['Alice 2']['grace'], 3600)
+        self.assertIn('http://', checks['Alice 2']['ping_url'])
+        self.assertEqual(checks['Alice 2']['status'], 'up')
+        self.assertEqual(self.now, parser.parse(checks['Alice 2']['last_ping']))
+        self.assertEqual(checks['Alice 2']['n_pings'], 0)
+        self.assertIn('pause', checks['Alice 2']['pause_url'])
 
     def test_it_shows_only_users_checks(self):
         bobs_check = Check(user=self.bob, name="Bob 1")
@@ -49,8 +68,16 @@ class ListChecksTestCase(BaseTestCase):
 
         r = self.get()
         data = r.json()
+
         self.assertEqual(len(data["checks"]), 2)
+
         for check in data["checks"]:
             self.assertNotEqual(check["name"], "Bob 1")
+            
+    def test_it_accepts_request_api_key(self):
+        r = self.client.generic("GET","/api/v1/checks/",
+                        json.dumps({"api_key": "abc"}),
+                        content_type="application/json")
 
-    ### Test that it accepts an api_key in the request
+        self.assertEqual(r.status_code, 200)
+        self.assertIn("Alice", r.content.decode("utf-8"))     
--- hc/api/tests/test_pause.py
+++ hc/api/tests/test_pause.py
@@ -1,6 +1,6 @@
 from hc.api.models import Check
 from hc.test import BaseTestCase
-
+import json
 
 class PauseTestCase(BaseTestCase):
 
@@ -12,7 +12,8 @@ class PauseTestCase(BaseTestCase):
         r = self.client.post(url, "", content_type="application/json",
                              HTTP_X_API_KEY="abc")
 
-        ### Assert the expected status code and check's status
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(json.loads(r.content.decode('utf-8'))["status"], "paused")
 
     def test_it_validates_ownership(self):
         check = Check(user=self.bob, status="up")
@@ -24,4 +25,24 @@ class PauseTestCase(BaseTestCase):
 
         self.assertEqual(r.status_code, 400)
 
-        ### Test that it only allows post requests
+    def test_it_allows_only_post(self):
+        check = Check(user=self.alice, status="up")
+        check.save()
+
+        url = "/api/v1/checks/%s/pause" % check.code
+
+        r = self.client.get(url, "", content_type="application/json",
+                             HTTP_X_API_KEY="abc")
+        self.assertEqual(r.status_code, 405)
+
+        r = self.client.put(url, "", content_type="application/json",
+                             HTTP_X_API_KEY="abc")
+        self.assertEqual(r.status_code, 405)
+
+        r = self.client.patch(url, "", content_type="application/json",
+                             HTTP_X_API_KEY="abc")
+        self.assertEqual(r.status_code, 405)
+
+        r = self.client.delete(url, "", content_type="application/json",
+                             HTTP_X_API_KEY="abc")
+        self.assertEqual(r.status_code, 405)
